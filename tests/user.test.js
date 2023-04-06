784d8a5310fd4ae163dd5525a3ab1897cecf0afd
aidas0Baranauskas <aidas0baranauskas@gmail.com>
2023-04-07 00:25:10 +0300

    init commit

--- tests/user.test.js
+++ tests/user.test.js
@@ -2,14 +2,24 @@ import request from "supertest";
 import mongoose from "mongoose";
 import app from "../src/app";
 
-test("Should signup a new user", async () => {
-	await request(app).post("/users").send({
-			name: "Aidas",
-			email: "b6@gmail.com",
-			password: "attemptingstuff"
+// test("Should signup a new user", async () => {
+// 	await request(app).post("/users").send({
+// 			name: "Aidas",
+// 			email: "b8@gmail.com",
+// 			password: "attemptingstuff"
+// 		})
+// 		.expect(201);
+// });
+test("Should login", async () => {
+	await request(app)
+		.post("/users/login")
+		.send({
+			email: "current@gmail.com",
+			password: "strongslaptazodis",
 		})
-		.expect(201);
+		.expect(200);
 });
+
 afterAll(async () => {
 	await mongoose.connection.close();
 });
