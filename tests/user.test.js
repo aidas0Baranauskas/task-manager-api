336d9c691675fa058d5cbf68e38c0fce19310d05
aidas0Baranauskas <aidas0baranauskas@gmail.com>
2023-04-06 23:30:36 +0300

    updating code

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,11 +1,15 @@
 import request from "supertest";
+import mongoose from "mongoose";
 import app from "../src/app";
 
 test("Should signup a new user", async () => {
 	await request(app).post("/users").send({
 			name: "Aidas",
-			email: "aidas5@gmail.com",
-			password: "passw0rd123",
+			email: "b6@gmail.com",
+			password: "attemptingstuff"
 		})
 		.expect(201);
+});
+afterAll(async () => {
+	await mongoose.connection.close();
 });
